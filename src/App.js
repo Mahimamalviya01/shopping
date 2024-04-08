f279461b0d3b1e49a38e6f5cb520928a848dc7f7
Mahimamalviya01 <mahi04malviya06@gmail.com>
2024-04-08 14:27:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+
+
+function App() {
+
+  return (
+   <div> 
+   <BrowserRouter>
+   <Navbar></Navbar>
+   
+   </BrowserRouter>
+
+   </div>
+  );
+  
+}
+
+export default App;
+
